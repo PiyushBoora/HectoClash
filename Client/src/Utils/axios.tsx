77e569ebfa6f46dbc6badec961792762675219fd
PiyushBoora <piyushkr1977@gmail.com>
2025-04-05 19:07:34 +0530

    changes

--- Client/src/Utils/axios.tsx
+++ Client/src/Utils/axios.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL="http://localhost:8080"
+// export const BASE_URL="http://localhost:8080"
+export const BASE_URL="https://hectoclash.onrender.com"
 const axiosInstance = axios.create({ baseURL: BASE_URL });
 axios.interceptors.request.use(request => {
   console.log('Starting Request', request)
